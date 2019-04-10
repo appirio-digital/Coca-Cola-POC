5a13e5a1fbb3fbee1cf6bca020e3d8072e72b93c
umangshu <umangshu.chouhan@appirio.com>
2019-04-10 11:57:46 +0530

    Hardcoded login

--- src/store/modules/login/actions.js
+++ src/store/modules/login/actions.js
@@ -1,6 +1,7 @@
 import { AsyncStorage } from 'react-native';
 import { LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_FAILED } from './types';
 import { CLEAR_STATE } from '../index';
+import sampleLoginJSON from './user.json';
 import {
   authenticateAnonymousMCS,
   authenticateMCS,
@@ -20,14 +21,11 @@ export const authenticateOAuthMCSUser = (email, password) => dispatch => {
   dispatch({
     type: LOGIN_USER
   });
-  authenticateMCS(email, password)
-    .then(result => {
-      console.log(result);
-      loginUserSuccess(dispatch, result);
-    })
-    .catch(error => {
-      loginUserFailed(dispatch, error);
-    });
+  if (email === 'Canadian.User' && password === 'Wipro@1234') {
+    loginUserSuccess(dispatch, sampleLoginJSON);
+  } else {
+    loginUserFailed(dispatch);
+  }
 };
 
 export const loginAnonymousUser = () => dispatch => {
@@ -51,11 +49,8 @@ export const logoutMCS = () => dispatch => {
     .then(errorCode => {});
 };
 
-const loginUserFailed = (dispatch, error) => {
-  let errorMessagePayload = 'Login Failed.';
-  if (error.message) {
-    errorMessagePayload = error.message;
-  }
+const loginUserFailed = dispatch => {
+  let errorMessagePayload = 'Username and password are incorrect.';
   dispatch({
     type: LOGIN_USER_FAILED,
     payload: errorMessagePayload
@@ -70,7 +65,7 @@ const clearState = dispatch => {
   });
 };
 
-const loginUserSuccess = async (dispatch, result) => {
+const loginUserSuccessOLD = async (dispatch, result) => {
   try {
     const {
       Minimum_Amount,
@@ -107,6 +102,72 @@ const loginUserSuccess = async (dispatch, result) => {
         };
         const profile = {
           ...response[0],
+          __ORACO__DistributionCentre_c: PriceBook,
+          ResourceProfileId: ResourceProfileId,
+          SR_Quantity: SR_Quantity,
+          Minimum_Amount: Minimum_Amount,
+          module: Module1,
+          PriceBook: PriceBook,
+          Prim: Prim,
+          RouteId: RouteId,
+          PrimaryCountry_c: Prim,
+          Currency: Currency
+        };
+        // const routeID = '/ORACO__Route_c/300000014886568/Attachment';
+        // const respo = createNewFile(
+        //   'Sample.jpeg',
+        //   'image/jpeg',
+        //   'file:///Users/umangshu/Library/Developer/CoreSimulator/Devices/35740041-2BE4-466A-A315-044902A8F29A/data/Containers/Data/Application/BC4F2B87-AF8C-4C34-8746-F214AB7BFE0F/Documents/300000014886247_LD Red Soft.jpg',
+        //   API_NAME,
+        //   routeID
+        // );
+
+        dispatch({
+          type: LOGIN_USER_SUCCESS,
+          payload: {
+            ...user,
+            profile: profile
+          }
+        });
+      } else {
+        loginUserFailed(dispatch, null);
+      }
+    }
+  } catch (error) {
+    loginUserFailed(dispatch, error.message);
+  }
+};
+
+const loginUserSuccess = async (dispatch, result) => {
+  try {
+    const {
+      Minimum_Amount,
+      Module1,
+      Prim,
+      RouteId,
+      SR_Quantity,
+      firstName,
+      lastName,
+      ResourceProfileId,
+      MobileAppIdentifier,
+      PriceBook,
+      Currency,
+      email
+    } = result;
+    if (result) {
+      if (!isEmpty(result)) {
+        labels.setLanguage(Prim == 'CA' ? 'en-IE' : 'en-IE');
+        updateCountryCode(Prim);
+        const user = {
+          user: {
+            userID: MobileAppIdentifier,
+            email: email,
+            firstName: firstName,
+            lastName: lastName
+          }
+        };
+        const profile = {
+          ...result,
           __ORACO__DistributionCentre_c: PriceBook,
           ResourceProfileId: ResourceProfileId,
           SR_Quantity: SR_Quantity,
